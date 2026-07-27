9505f63dbfc3a489edfcfdc3588c4df156cf572f
ricci1mmm <76752981+ricci1mmm@users.noreply.github.com>
2026-07-27 23:10:36 +0300

    Create game entry point

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../game/core/Character.php';
+require_once __DIR__ . '/../game/world/District.php';
+
+use Game\Core\Character;
+use Game\World\District;
+
+echo 'Football Hooligans V2';
